9315e71b0b202a07244bbec2fea9212d66e3fc60
San2855 <santiago.henrique@ufrpe.br>
2022-12-09 08:52:20 -0300

    Matriz de hilbert

--- Hilbert.py
+++ Hilbert.py
@@ -1,14 +1,3 @@
-import math
-a = int(input("Insira o número de linhas"))
-b = int(input("Insira o número de colunas"))
-
-def hilmat(a,b):
-    li=[[0]]*a
-    for i in range(a):
-        for j in range(b):
-            ele=math.pow((i+j+1),-1)
-            li[i].append(ele)
-    return li
-
-
-print(hilmat(a,b))
+def hilmat(a, b):
+    return [[1 / (i + j + 1) for j in range(b)] for i in range(a)]
+print(hilmat(3, 3))
